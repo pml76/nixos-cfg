9b1224be70c7a4fb882af2cae88764689aeb8534
Peter Lennartz <peter.lennartz[at]gmail.com>
2026-03-15 17:38:21 +0100

    .

--- workstations/work/flake.nix
+++ workstations/work/flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS flake for my work-machine's VM";
 
   inputs = {
-    nixpkgs.url = "github.com/NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github.com/NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
