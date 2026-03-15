477fc84fba437369b1302852e25dc12c01cf8057
Peter Lennartz <peter.lennartz[at]gmail.com>
2026-03-15 18:02:47 +0100

    switched to unstable

--- workstations/work/flake.nix
+++ workstations/work/flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS flake for my work-machine's VM";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
